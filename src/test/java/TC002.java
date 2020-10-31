05207e399c432256d5b65661c9c4a9fe89d612e4
Nurbiye <nurbiye720@gmail.com>
2020-10-31 14:36:53 -0500

    TC001&TC002 files

---
+++ src/test/java/TC002.java
@@ -0,0 +1,5 @@
+public class TC002 {
+    public static void main(String[] args) {
+        System.out.println("each weekend is little hard for me, since kids are all at home and I have full day class");
+    }
+}
