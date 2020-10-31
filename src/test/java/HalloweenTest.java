3823f5fd3b94e155b0c0598f085da65e2c0ce6e8
Nurbiye <nurbiye720@gmail.com>
2020-10-31 11:43:09 -0500

    update

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -1,4 +1,11 @@
 public class HalloweenTest {
 
     //actually this is just for test
+    //we created a new project and created class both in main and test under java
+
+
+    //right click the file-->git--> add to gitignore-----> this is also the way to put the file into git ignore
+    //but before doing all these, we should create or initialize git/version control by enableing version control in VCS
+    //this is use for one time for each project, later you don't see that function in VCS
+
 }
